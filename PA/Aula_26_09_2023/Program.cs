f05ea6565df8ca6a5a531518e13100b442b577d7
Aluno <aluno@etec.sp.gov.br>
2023-09-26 22:48:31 -0300

    Atividade Aula 26/09/2023

--- PA/Aula_26_09_2023/Program.cs
+++ PA/Aula_26_09_2023/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace Aula_19_09_2023
+namespace Aula_26_09_2023
 {
     class Program
     {
         static void Main(string[] args)
         {
-            float n1, n2, n3, n4, media;
+            float n1, n2, n3, n4, media, percFreq;
 
             Console.Write("Informe sua 1° Nota: ");
             n1 = float.Parse(Console.ReadLine());
@@ -17,23 +17,23 @@ namespace Aula_19_09_2023
             Console.Write("Informe sua 4° Nota: ");
             n4 = float.Parse(Console.ReadLine());
 
+            Console.Write("Informe o % de Frequência: ");
+            percFreq = float.Parse(Console.ReadLine());
+
             media = (n1 + n2 + n3 + n4) / 4;
 
-            if (media < 5)
+            if (media < 5 && percFreq <= 40)
             {
-                Console.WriteLine("Reprovado :C");
-                Console.WriteLine("Meuuu deuss...");
-
-            }
-            else if (media < 7)
+                Console.WriteLine("Aluno Reprovado");
+            } else if (media < 7 && percFreq <= 60)
             {
-                Console.WriteLine("Recuperação :C");
-                Console.WriteLine("Já pode desistir...");
-            } 
-            else 
+                Console.WriteLine("Aluno de Recuperação");
+            } else if (media <= 10 && percFreq > 60)
             {
-                Console.WriteLine("Aprovado ;D");
-                Console.WriteLine("Parabens GAROTÃO!!");
+                Console.WriteLine("Aluno Aprovado");
+            } else
+            {
+                Console.WriteLine("Será analisado pela secretária acadêmica");
             }
         }
     }
