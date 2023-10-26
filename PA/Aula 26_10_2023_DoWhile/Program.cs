b3dda6221bed62fd13f8c0f7026ca3f678ac2c2c
Aluno <aluno@etec.sp.gov.br>
2023-10-26 20:49:31 -0300

    Aula 26/10/2023

---
+++ PA/Aula 26_10_2023_DoWhile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aula_26_10_2023_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+
+            do
+            {
+                Console.WriteLine($"2 x {contador} = {2 * contador}");
+                contador++;
+            } while (contador <= 10);
+        }
+    }
+}
---
+++ PA/Aula 26_10_2023_DoWhile2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula_26_10_2023_DoWhile2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float valorFinanciado;
+            int qtdParcela, contador = 1;
+
+            Console.Write("Informe o valor financiado: ");
+            valorFinanciado = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe a Qtd de parcelas: ");
+            qtdParcela = int.Parse(Console.ReadLine());
+
+            DateTime data = DateTime.Now.Date;
+
+            while (contador <= qtdParcela)
+            {
+                Console.WriteLine($"{contador}ª parcela ({data}): {valorFinanciado / qtdParcela:0.00}");
+                contador++;
+                data = data.AddMonths(1);
+            }
+
+        }
+    }
+}
