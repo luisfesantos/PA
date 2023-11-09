65bc0148ae3e48155fe16efc819657ea71bab87f
Aluno <aluno@etec.sp.gov.br>
2023-11-09 20:51:37 -0300

    Aula 09/11/2023

---
+++ PA/Aula_09_11_2023_ExWhile/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Aula_09_11_2023_ExWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resposta = "S";
+
+            while (resposta.ToUpper() == "S")
+            {
+                Console.Clear();
+
+                CalcularTabuada();
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Deseja continuar? ");
+                resposta = Console.ReadLine();
+            }
+            
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("*** FIM ***");
+
+        }
+
+        static void CalcularTabuada()
+        {
+            int fator;
+
+            Console.Write("Informe o fator desejado: ");
+            fator = int.Parse(Console.ReadLine());
+
+            for (int contador = 1; contador <= 10; contador++)
+            {
+
+                if (contador % 2 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+
+                Console.WriteLine($"{fator} x {contador} = {fator * contador}");
+            }
+        }
+    }
+}
