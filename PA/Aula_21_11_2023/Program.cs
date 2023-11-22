73177d71d95347d9199d2db92588ee6ae3e623bd
Aluno <aluno@etec.sp.gov.br>
2023-11-21 22:45:24 -0300

    Aula 21/11/2023

---
+++ PA/Aula_21_11_2023/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula_21_11_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura, alturaMA = 0, alturaMN = 0;
+            for (int pessoas = 1; pessoas <= 15; pessoas++)
+            {
+                Console.Write("Informe altura:");
+                altura = float.Parse(Console.ReadLine());
+
+                if (altura > alturaMA)
+                {
+                    alturaMA = altura;
+                }
+                if (altura < alturaMN || alturaMN == 0)
+                {
+                    alturaMN = altura;
+                }
+            }
+
+            Console.WriteLine($"A Maior altura é: {alturaMA} e a Menor é: {alturaMN}");
+        }
+    }
+}
---
+++ PA/Aula_21_11_2023_ExForIF2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Aula_21_11_2023_ExForIF2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int voto, cand1 = 0, cand2 = 0, cand3 = 0, cand4 = 0, nulo = 0, branco = 0;
+
+            for (;;)
+            {
+                Console.Write("Informe seu voto: ");
+                voto = int.Parse(Console.ReadLine());
+
+                if (voto == 0)
+                {
+                    break;
+                }
+                if (voto == 1)
+                {
+                    cand1++;
+                } 
+                if (voto == 2)
+                {
+                    cand2++;
+                }
+                if (voto == 3)
+                {
+                    cand3++;
+                }
+                if (voto == 4)
+                {
+                    cand4++;
+                }
+                if (voto == 5)
+                {
+                    nulo++;
+                }
+                if (voto == 6)
+                {
+                    branco++;
+                }
+
+            }
+
+                Console.WriteLine($"Candidato 1º: {cand1} Votos");
+                Console.WriteLine($"Candidato 2º: {cand2} Votos");
+                Console.WriteLine($"Candidato 3º: {cand3} Votos");
+                Console.WriteLine($"Candidato 4º: {cand4} Votos");
+                Console.WriteLine($"Votos Nulos: {nulo} Votos");
+                Console.WriteLine($"Votos em Branco: {branco} Votos");
+        }
+    }
+}
